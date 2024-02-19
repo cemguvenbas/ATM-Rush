48a381729f6e1af6beba8f8c9ada9be4674281dc
Cem Güvenbaş <cemguvenbass@gmail.com>
2024-02-19 14:58:27 +0300

    Update CoreGameSignals

--- ATM Rush/Assets/Scripts/Runtime/Signals/CoreGameSignals.cs
+++ ATM Rush/Assets/Scripts/Runtime/Signals/CoreGameSignals.cs
@@ -6,10 +6,21 @@ using UnityEngine.Events;
 
 public class CoreGameSignals : MonoSingleton<CoreGameSignals>
 {
+    //public UnityAction<GameStates> onChangeGameStates = delegate { };
     public UnityAction<byte> onLevelInitialize = delegate { };
     public UnityAction onClearActiveLevel = delegate { };
     public Func<byte> onGetLevelValue = delegate { return 0; };
     public UnityAction onNextLevel = delegate { };
     public UnityAction onRestartLevel = delegate { };
     public UnityAction onReset = delegate { };
+    public UnityAction onPlay = delegate { };
+    public UnityAction onLevelSuccessful = delegate { };
+    public UnityAction onLevelFailed = delegate { };
+
+    public UnityAction onMiniGameEntered = delegate { };
+    public UnityAction<GameObject> onAtmTouched = delegate { };
+    public UnityAction onMiniGameStart = delegate { };
+
+    public Func<byte> onGetIncomeLevel = delegate { return 0; };
+    public Func<byte> onGetStackLevel = delegate { return 0; };
 }
